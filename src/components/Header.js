7e423c11eb752fcde43831d579edfce313a49200
Dmitry Rusinov <kachek96@yandex.ru>
2023-08-06 16:34:12 +0300

    редактирован README

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import headerLogo from "../images/logo.svg";
 export default function Header() {
   return (
     <header className="header">
-      <img src={headerLogo} className="header__logo" />
+      <img alt="Логотип" src={headerLogo} className="header__logo" />
     </header>
   );
 }
